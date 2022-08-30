8e6b77a4806779a7dca776ad265c4ff8d6f3047d
tpiras <thomas.piras@enalean.com>
2022-08-30 14:02:15 +0200

    Build in release mode

--- wasmtime-ffi/wasmtimewrapper.h
+++ wasmtime-ffi/wasmtimewrapper.h
@@ -1,4 +1,4 @@
 #define FFI_SCOPE "libwasmtimewrapper"
-#define FFI_LIB "../wasmtime-wrapper-lib/target/debug/libwasmtimewrapper.so"
+#define FFI_LIB "../wasmtime-wrapper-lib/target/release/libwasmtimewrapper.so"
 
 const char * compile_and_exec(const char *filename, const char *funcname);
